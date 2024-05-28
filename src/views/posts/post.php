f9a12da5d010bfcac6f8634b60bc6019ca0f84b4
salvador-diaz <s4lv4dord@gmail.com>
2024-05-27 22:17:42 -0300

    Agrega viewport meta tag para aplicar reglas css segun dispositivo

--- src/views/posts/post.php
+++ src/views/posts/post.php
@@ -1,17 +1,17 @@
 <html>
     <head>
-        <link rel="stylesheet" href="src/views/assets/general.css">
-        <link rel="stylesheet" href="src/views/assets/buttons.css">
-        <link rel="stylesheet" href="src/views/assets/nav.css">
-        <link rel="stylesheet" href="src/views/assets/post.css">
-
         <link rel="icon" type="image/png" href="src/storage/icon.png">
-
         <!-- Archivo Narrow font -->
         <link rel="preconnect" href="https://fonts.googleapis.com">
         <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
         <link href="https://fonts.googleapis.com/css2?family=Archivo+Narrow:ital,wght@0,400..700;1,400..700&display=swap" rel="stylesheet">
         <!-- -->
+
+        <meta name="viewport" content="width=device-width, initial-scale=1">
+        <link rel="stylesheet" href="src/views/assets/general.css">
+        <link rel="stylesheet" href="src/views/assets/buttons.css">
+        <link rel="stylesheet" href="src/views/assets/nav.css">
+        <link rel="stylesheet" href="src/views/assets/post.css">
     </head>
     <body>
         <?php include RUTA_VISTAS."/components/nav.php"; ?>
